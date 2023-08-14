b8f9e0c90ff869c974abcb006b39b85def1a2bc0
Dmitri9149 <dmitri.as.name@gmail.com>
2023-08-14 12:31:47 +0300

    Add more refs to html file

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,6 @@ fn main() {
     }
 }
 
-/*
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-    
-        let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
-}
-*/
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
@@ -43,6 +23,8 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 200 OK", "paths/hello.html")
     } else if request_line == "GET /news HTTP/1.1" {
         ("HTTP/1.1 200 OK", "paths/news.html")
+    } else if request_line == "GET /resources HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "paths/resources.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "paths/404.html")
     };
